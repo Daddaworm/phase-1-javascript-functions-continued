d5e9082e51a90b0683f33c21ae4187235869f4a9
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-04-28 15:21:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(char='*'){ // implement a fuction called wrapAdjective
+    return function(msg=`special`){
+        return `You are ${char}${msg}${char}!` //should return 'You are *a hard worker*!'
+    }
+}
